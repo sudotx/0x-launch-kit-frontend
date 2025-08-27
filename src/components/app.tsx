5841cd14ef549e5d8ee95a2b52c28fe88e70d059
sudotx <64108848+sudotx@users.noreply.github.com>
2025-08-27 15:16:11 +0100

    refactor: enhance app structure with RainbowKit and Wagmi integration, streamline component logic

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,122 +1,82 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import { useLocation, useNavigate } from 'react-router-dom';
+import React, { useCallback, useEffect, useRef } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
 import { UI_UPDATE_CHECK_INTERVAL, UPDATE_ETHER_PRICE_INTERVAL } from '../common/constants';
 import { LocalStorage } from '../services/local_storage';
 import { initializeAppNoMetamaskOrLocked, initWallet, updateMarketPriceEther, updateStore } from '../store/actions';
 import { getWeb3State } from '../store/selectors';
-import { StoreState, Web3State } from '../util/types';
+import { Web3State } from '../util/types';
+import { AppDispatch } from '../store';
 
 interface OwnProps {
     children: React.ReactNode;
 }
 
-interface StateProps {
-    web3State: Web3State;
-}
-
-interface DispatchProps {
-    onConnectWallet: () => any;
-    onInitMetamaskState: () => any;
-    onUpdateStore: () => any;
-    onUpdateMarketPriceEther: () => any;
-}
-
-type Props = OwnProps & DispatchProps & StateProps;
-
 const localStorage = new LocalStorage(window.localStorage);
 
-// Create a wrapper component to use hooks
-const AppWithRouter = (props: Props) => {
-    const location = useLocation();
-    const navigate = useNavigate();
+const App: React.FC<OwnProps> = ({ children }) => {
+    const dispatch = useDispatch<AppDispatch>();
+    const web3State = useSelector(getWeb3State);
 
-    return <App {...props} location={location} navigate={navigate} />;
-};
+    const updateStoreInterval = useRef<number | undefined>(undefined);
+    const updatePriceEtherInterval = useRef<number | undefined>(undefined);
 
-class App extends React.Component<Props & { location: any; navigate: any }> {
-    private _updateStoreInterval: number | undefined;
-    private _updatePriceEtherInterval: number | undefined;
+    const onConnectWallet = useCallback(() => dispatch(initWallet()), [dispatch]);
+    const onInitMetamaskState = useCallback(() => dispatch(initializeAppNoMetamaskOrLocked()), [dispatch]);
+    const onUpdateStore = useCallback(() => dispatch(updateStore()), [dispatch]);
+    const onUpdateMarketPriceEther = useCallback(() => dispatch(updateMarketPriceEther()), [dispatch]);
 
-    public componentDidMount = () => {
-        const wasWalletConnected = localStorage.getWalletConnected();
-        if (wasWalletConnected) {
-            this.props.onConnectWallet();
-        } else {
-            this.props.onInitMetamaskState();
+    const deactivatePollingUpdates = useCallback(() => {
+        if (updateStoreInterval.current) {
+            clearInterval(updateStoreInterval.current);
+            updateStoreInterval.current = undefined;
         }
-    };
 
-    public componentDidUpdate = async (prevProps: Readonly<Props>, prevState: Readonly<Props>, snapshot?: any) => {
-        const { web3State } = this.props;
-        if (web3State !== prevProps.web3State) {
-            if (web3State === Web3State.Done) {
-                this._activatePollingUpdates();
-            } else {
-                // If the user is currently using the dApp with the interval and the metamask status changed, the polling is removed
-                this._deactivatePollingUpdates();
-            }
+        if (updatePriceEtherInterval.current) {
+            clearInterval(updatePriceEtherInterval.current);
+            updatePriceEtherInterval.current = undefined;
         }
-    };
+    }, []);
 
-    public componentWillUnmount = () => {
-        clearInterval(this._updateStoreInterval);
-        clearInterval(this._updatePriceEtherInterval);
-    };
-
-    public render = () => this.props.children;
-
-    private readonly _activatePollingUpdates = () => {
-        // Enables realtime updates of the store using polling
-        if (UI_UPDATE_CHECK_INTERVAL !== 0 && !this._updateStoreInterval) {
-            this._updateStoreInterval = window.setInterval(async () => {
-                this.props.onUpdateStore();
-                this.setState({
-                    isActiveCheckUpdates: true,
-                });
+    const activatePollingUpdates = useCallback(() => {
+        if (UI_UPDATE_CHECK_INTERVAL !== 0 && !updateStoreInterval.current) {
+            updateStoreInterval.current = window.setInterval(() => {
+                onUpdateStore();
             }, UI_UPDATE_CHECK_INTERVAL);
         }
 
-        // Enables realtime updates of the price ether using polling
-        if (!this._updatePriceEtherInterval && UPDATE_ETHER_PRICE_INTERVAL !== 0) {
-            this._updatePriceEtherInterval = window.setInterval(async () => {
-                this.props.onUpdateMarketPriceEther();
+        if (!updatePriceEtherInterval.current && UPDATE_ETHER_PRICE_INTERVAL !== 0) {
+            updatePriceEtherInterval.current = window.setInterval(() => {
+                onUpdateMarketPriceEther();
             }, UPDATE_ETHER_PRICE_INTERVAL);
         }
-    };
+    }, [onUpdateStore, onUpdateMarketPriceEther]);
 
-    private readonly _deactivatePollingUpdates = () => {
-        if (this._updateStoreInterval) {
-            clearInterval(this._updateStoreInterval);
-            this._updateStoreInterval = undefined;
+    useEffect(() => {
+        const wasWalletConnected = localStorage.getWalletConnected();
+        if (wasWalletConnected) {
+            onConnectWallet();
+        } else {
+            onInitMetamaskState();
         }
+    }, [onConnectWallet, onInitMetamaskState]);
 
-        if (this._updatePriceEtherInterval) {
-            clearInterval(this._updatePriceEtherInterval);
-            this._updatePriceEtherInterval = undefined;
+    useEffect(() => {
+        if (web3State === Web3State.Done) {
+            activatePollingUpdates();
+        } else {
+            deactivatePollingUpdates();
         }
-    };
-}
+    }, [web3State, activatePollingUpdates, deactivatePollingUpdates]);
 
-const mapStateToProps = (state: StoreState): StateProps => {
-    return {
-        web3State: getWeb3State(state),
-    };
+    useEffect(() => {
+        return () => {
+            clearInterval(updateStoreInterval.current);
+            clearInterval(updatePriceEtherInterval.current);
+        };
+    }, []);
+
+    return <>{children}</>;
 };
 
-const mapDispatchToProps = (dispatch: any) => {
-    return {
-        onInitMetamaskState: () => dispatch(initializeAppNoMetamaskOrLocked()),
-        onUpdateStore: () => dispatch(updateStore()),
-        onUpdateMarketPriceEther: () => dispatch(updateMarketPriceEther()),
-        onConnectWallet: () => dispatch(initWallet()),
-    };
-};
-
-const AppContainer = connect(
-    mapStateToProps,
-    mapDispatchToProps,
-)(AppWithRouter);
-
-export { App, AppContainer };
+export default App
--- src/index.tsx
+++ src/index.tsx
@@ -3,19 +3,46 @@ import { Provider } from 'react-redux';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import 'sanitize.css';
 
+import { ConnectButton, getDefaultConfig, RainbowKitProvider } from '@rainbow-me/rainbowkit';
+import "@rainbow-me/rainbowkit/styles.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { http, WagmiProvider } from "wagmi";
+import { sepolia } from "wagmi/chains";
 import { ERC20_APP_BASE_PATH, ERC721_APP_BASE_PATH, LOGGER_ID } from './common/constants';
-import { AppContainer } from './components/app';
+import App from './components/app';
 import { Erc20App } from './components/erc20/erc20_app';
 import { Erc721App } from './components/erc721/erc721_app';
 import './index.css';
-import * as serviceWorker from './serviceWorker';
 import { store } from './store';
 
-// ReactModal.setAppElement('#root');
+const queryClient = new QueryClient({
+	defaultOptions: {
+		queries: {
+			staleTime: 30_000, // 30 seconds for DeFi data
+			gcTime: 1_000 * 60 * 60 * 24, // 24 hours
+			networkMode: 'offlineFirst',
+			refetchOnWindowFocus: false,
+			retry: 0,
+		},
+		mutations: { networkMode: 'offlineFirst' },
+	}
+});
 
-if (['development', 'production'].includes(process.env.NODE_ENV) && !window.localStorage.debug) {
+
+export const config = getDefaultConfig({
+	appName: "Exchange",
+	projectId: process.env.PROJECT_ID || "8X1df9Wbcqj6A7LWG71Ra5yLYj-1eL7y",
+	chains: [sepolia],
+	transports: {
+		[sepolia.id]: http(),
+	}
+});
+
+ReactModal.setAppElement('#root');
+
+if (['development', 'production'].includes(process.env.NODE_ENV || 'development') && !window.localStorage.debug) {
 	// Log only the app constant id to the console
 	window.localStorage.debug = `${LOGGER_ID}*`;
 }
@@ -23,24 +50,32 @@ if (['development', 'production'].includes(process.env.NODE_ENV) && !window.loca
 function Home() {
 	return (
 		<div>
-			<h1>Hello</h1>
+			<ConnectButton />
+			<h1>Hello Exchange</h1>
 		</div>
 	)
 }
 
+
 function Web3WrappedApp() {
 	return (
-		<Provider store={store}>
-			<BrowserRouter>
-				<AppContainer>
-					<Routes>
-						<Route path={ERC20_APP_BASE_PATH} element={<Erc20App />} />
-						<Route path={ERC721_APP_BASE_PATH} element={<Erc721App />} />
-						<Route path="/" element={<Home />} />
-					</Routes>
-				</AppContainer>
-			</BrowserRouter>
-		</Provider>
+		<WagmiProvider config={config}>
+			<QueryClientProvider client={queryClient}>
+				<RainbowKitProvider>
+					<Provider store={store}>
+						<BrowserRouter>
+							<App>
+								<Routes>
+									<Route path={ERC20_APP_BASE_PATH} element={<Erc20App />} />
+									<Route path={ERC721_APP_BASE_PATH} element={<Erc721App />} />
+									<Route path="/" element={<Home />} />
+								</Routes>
+							</App>
+						</BrowserRouter>
+					</Provider>
+				</RainbowKitProvider>
+			</QueryClientProvider>
+		</WagmiProvider>
 	)
 }
 
@@ -49,8 +84,3 @@ createRoot(document.getElementById('root')!).render(
 		<Web3WrappedApp />
 	</StrictMode>
 )
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
